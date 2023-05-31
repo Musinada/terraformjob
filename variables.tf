f48ba03c3e0324be0e3d241a5e5c2570da2df3e6
Anil Kumar Musinada <121026687+Musinada@users.noreply.github.com>
2023-05-31 21:19:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "linux_ami_id" {
     description = "ami id of the linux machine"
-    default = "ami-052efd3df9dad4825"
+    default = "ami-0607784b46cbe5816"
 }
 variable "vpc_id" {
     description = "vpc_id under which the resource group will create"
-    default = "vpc-082485d83a32d7336"
+    default = "vpc-0712f5b29ecc1e69f"
 }
 variable "subnet" {
     description = "subnet id of the vpc"
-    default = "	subnet-0b09b80c31a47edbf"
+    default = "subnet-0843de099cd275280"
 }
 variable "cidr_blocks" {
     description = "subnet ip range in cidr block"
@@ -17,7 +17,7 @@ variable "cidr_blocks" {
 
 variable "region" {
     description = "aws region"
-    default = "us-east-1"
+    default = "ap-south-1"
 }
 variable "volume_type" {
     description = "type of the storage"
